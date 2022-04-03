63818061531a2759950bbd27761156e186da8f7c
Mantas <m.juozenas@gmail.com>
2022-04-03 19:44:17 +0300

    Task 3 done

--- Task 3/script.js
+++ Task 3/script.js
@@ -11,3 +11,30 @@ Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų 
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'https://api.github.com/users';
+const showUsersBtn = document.getElementById('btn');
+const divEl = document.getElementById('output');
+
+showUsersBtn.addEventListener('click', fetchData);
+
+async function fetchData() {
+  try {
+    const res = await fetch(ENDPOINT);
+    const data = await res.json();
+    generateCards(data, divEl);
+  } catch (error) {
+    console.log(`Something went wrong. ERROR MESSAGE: ${error.message}`);
+  }
+}
+
+function generateCards(dataObj, dest) {
+  dest.innerHTML = '';
+  dataObj.forEach((user) => {
+    const string = `
+        <div class="card">
+            <h2 class="login">${user.login}</h2>
+            <img src="${user.avatar_url}" alt="Picture of ${user.login}" />
+        </div>
+        `;
+    dest.insertAdjacentHTML('beforeend', string);
+  });
+}
