e64890b43d6cd514fe9a0a331db4330160dc2bcd
Mantas <m.juozenas@gmail.com>
2022-04-03 20:09:47 +0300

    Task 1 done

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,35 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+{
+}
+
+const outputEl = document.getElementById('output');
+const inputEl = document.getElementById('search');
+const btnEl = document.getElementById('submit-btn');
+
+btnEl.addEventListener('click', (e) => {
+  e.preventDefault();
+  return calculate(outputEl, inputEl);
+});
+
+function calculate(dest, input) {
+  dest.innerHTML = '';
+  const lb = input.value * 2.2046;
+  const g = input.value / 0.001;
+  const oz = input.value * 35.274;
+  const string = `
+    <div class="lb">
+        <h2>${input.value} kg = ${lb} svarai(lb)</h2>
+    </div>
+    <div class="g">
+        <h2>${input.value} kg = ${g} gramai(g)</h2>
+    </div>
+    <div class="oz">
+        <h2>${input.value} kg = ${oz} uncijos(oz)</h2>
+    </div>
+  `;
+  dest.insertAdjacentHTML('beforeend', string);
+  input.value = '';
+}
